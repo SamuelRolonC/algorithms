190061982d3c79f5c04921db32b5bbcc712af2e1
Samuel Rolon Cicciari <samuelrolonc@gmail.com>
2023-03-27 20:27:49 -0300

    First version linkedlist and array
    - Added a first iteration for DoubleLinkedListInt and other try for a
    generic DoubleLinkedList.
    - Added a first iteration for a Array of integers.

---
+++ data-structure/DoubleLinkedListInt.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    using System.Collections;
+
+    namespace DynamicArray
+    {
+        public class DoublyLinkedListInt : IEnumerable
+        {
+            private int Size = 0;
+            private Node? Head;
+            private Node? Tail;
+
+            public void Clear()
+            {
+                var traverser = Head;
+                while (traverser != null)
+                {
+                    var next = traverser.Next;
+                    traverser.Previous = null;
+                    traverser.Next = null;
+                    traverser.Data = 0;
+                    traverser = next;
+                }
+                Head = null;
+                Tail = null;
+                Size = 0;
+            }
+
+            public int GetSize()
+            {
+                return Size;
+            }
+
+            public bool IsEmpty()
+            {
+                return Size == 0;
+            }
+
+            public void Add(int element)
+            {
+                AddLast(element);
+            }
+
+            public void AddLast(int element)
+            {
+                if (IsEmpty())
+                {
+                    Head = new Node(element, null, null);
+                    Tail = new Node(element, null, null);
+                }
+                else
+                {
+                    if (Tail == null) throw new NullNodeLinkedListException("tail");
+                    Tail.Next = new Node(element, Tail, null);
+                    Tail = Tail?.Next;
+                }
+                Size++;
+            }
+
+            public void AddFirst(int element)
+            {
+                if (IsEmpty())
+                {
+                    Head = new Node(element, null, null);
+                    Tail = new Node(element, null, null);
+                }
+                else
+                {
+                    if (Head == null) throw new NullNodeLinkedListException("head");
+                    Head.Previous = new Node(element, null, Head);
+                    Head = Head.Previous;
+                }
+                Size++;
+            }
+
+            public void AddAt(int index, int data)
+            {
+                if (index < 0 || index >= Size)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                if (index == 0)
+                {
+                    AddFirst(data);
+                    return;
+                }
+                else if (index == Size - 1)
+                {
+                    AddLast(data);
+                    return;
+                }
+
+                var temp = Head;
+                for (var i = 0; i < index - 1; i++)
+                {
+                    temp = temp?.Next;
+                }
+                var newNode = new Node(data, temp, temp?.Next);
+                if(temp?.Next == null) throw new NullReferenceException($"Null node was found while adding a element to the list.");
+                temp.Next.Previous = newNode;
+                temp.Next = newNode;
+
+                Size++;
+            }
+
+            public int PeekFirst()
+            {
+                if (IsEmpty()) throw new EmptyLinkedListException();
+                if (Head == null) throw new NullNodeLinkedListException("head");
+                return Head.Data;
+            }
+
+            public int PeekLast()
+            {
+                if (IsEmpty()) throw new EmptyLinkedListException();
+                if (Tail == null) throw new NullNodeLinkedListException("tail");
+                return Tail.Data;
+            }
+
+            public int removeFirst()
+            {
+                if (IsEmpty()) throw new EmptyLinkedListException();
+
+                if (Head == null) throw new NullNodeLinkedListException("head");
+                var data = Head.Data;
+                Head = Head.Next;
+                --Size;
+
+                if (IsEmpty())
+                    Tail = null;
+                else
+                {
+                    if (Head == null) throw new NullNodeLinkedListException("head");
+                    Head.Previous = null;
+                }
+
+                return data;
+            }
+
+            public int removeLast()
+            {
+                if (IsEmpty()) throw new EmptyLinkedListException();
+
+                if (Tail == null) throw new NullNodeLinkedListException("tail");
+                var data = Tail.Data;
+                Tail = Tail.Next;
+                --Size;
+
+                if (IsEmpty())
+                    Tail = null;
+                else
+                {
+                    if (Tail == null) throw new NullNodeLinkedListException("tail");
+                    Tail.Previous = null;
+                }
+
+                return data;
+            }
+
+            private int Remove(Node node)
+            {
+                if (node == null) throw new ArgumentNullException(nameof(node));
+
+                if (node.Previous == null) return removeFirst();
+                if (node.Next == null) return removeLast();
+
+                node.Next.Previous = node.Previous;
+                node.Previous.Next = node.Next;
+
+                var data = node.Data;
+                node.Previous = null;
+                node.Next = null;
+
+                --Size;
+
+                return data;
+            }
+
+            public int RemoveAt(int index)
+            {
+                if (index < 0 || index >= Size)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+
+                int i;
+                Node? traverser;
+
+                if (index < Size / 2)
+                {
+                    if (Head == null) throw new NullNodeLinkedListException("head");
+                    for (i = 0, traverser = Head; i != index; i++)
+                    {
+                        traverser = traverser?.Next;
+                    }
+                }
+                else
+                {
+                    if (Tail == null) throw new NullNodeLinkedListException("tail");
+                    for (i = Size - 1, traverser = Tail; i != index; i--)
+                    {
+                        traverser = traverser?.Previous;
+                    }
+                }
+                if(traverser == null) throw new NullReferenceException($"Null node was found while removing a element of the list.");
+                return Remove(traverser);
+            }
+
+            public bool Remove(int value)
+            {
+                var traverser = Head;
+
+                for(traverser = Head; traverser != null; traverser = traverser.Next)
+                {
+                    if (value == traverser.Data)
+                    {
+                        Remove(traverser);
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public int IndexOf(int value)
+            {
+                var index = 0;
+                var traverser = Head;
+
+                for(;traverser != null; traverser = traverser.Next, index++)
+                {
+                    if (value == traverser.Data)
+                    {
+                        return index;
+                    }
+                }
+                return -1;
+            }
+
+            public bool Contains(int value)
+            {
+                return IndexOf(value) >= 0;
+            }
+
+            public IEnumerator<int> GetEnumerator()
+            {
+                var index = 0;
+                var traverser = Head;
+
+                for (; traverser != null; traverser = traverser.Next, index++)
+                {
+                    yield return traverser.Data;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            public override string ToString()
+            {
+                if (Head == null) throw new NullNodeLinkedListException("head");
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("[ ");
+                Node traverser = Head;
+
+                while (traverser != null)
+                {
+                    sb.Append(traverser.Data);
+                    if (traverser.Next != null)
+                        sb.Append(", ");
+                    traverser = traverser?.Next;
+                }
+                sb.Append(" ]");
+                return sb.ToString();
+            }
+        }
+
+        internal class Node
+        {
+            public int Data;
+            public Node? Previous;
+            public Node? Next;
+
+            public Node(int data, Node? previous, Node? next)
+            {
+                Data = data;
+                Previous = previous;
+                Next = next;
+            }
+
+            override public string ToString()
+            {
+                return Data.ToString();
+            }
+        }
+
+        [Serializable]
+        public class EmptyLinkedListException : Exception
+        {
+            public EmptyLinkedListException() : base("Trying to access an empty list.") { }
+        }
+
+        [Serializable]
+        public class NullNodeLinkedListException : Exception
+        {
+            public NullNodeLinkedListException(string node) : base($"List's {node} node is null.") { }
+        }
+    }
+
+}
---
+++ data-structure/DoublyLinkedList.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+
+namespace DynamicArray
+{
+    public class DoublyLinkedList<T> : IEnumerable<T> where T : class
+    {
+        private int Size = 0;
+        private Node<T>? Head;
+        private Node<T>? Tail;
+
+        public void Clear()
+        {
+            var traverser = Head;
+            while(traverser != null)
+            {
+                var next = traverser.Next;
+                traverser.Previous = null;
+                traverser.Next = null;
+                traverser.Data = null;
+                traverser = next;
+            }
+            Head = null;
+            Tail = null;
+            traverser = null;
+            Size = 0;
+        }
+
+        public int GetSize()
+        {
+            return Size;
+        }
+
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
+
+        public void Add(T element)
+        {
+            AddLast(element);
+        }
+
+        public void AddLast(T element)
+        {
+            if (IsEmpty())
+            {
+                Head = new Node<T>(element, null, null);
+                Tail = new Node<T>(element, null, null);
+            }
+            else
+            {
+                Tail.Next = new Node<T>(element, Tail, null);
+                Tail = Tail.Next;
+            }
+            Size++;
+        }
+
+        public void AddFirst(T element)
+        {
+            if (IsEmpty())
+            {
+                Head = new Node<T>(element, null, null);
+                Tail = new Node<T>(element, null, null);
+            }
+            else
+            {
+                Head.Previous = new Node<T>(element, null, Head);
+                Head = Head.Previous;
+            }
+            Size++;
+        }
+
+        public void AddAt(int index, T data)
+        {
+            if (index < 0 || index >= Size)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (index == 0)
+            {
+                AddFirst(data);
+                return;
+            } 
+            else if (index == Size - 1)
+            {
+                AddLast(data);
+                return;
+            }
+
+            var temp = Head;
+            for(var i = 0; i < index - 1; i++)
+            {
+                temp = temp?.Next;
+            }
+            var newNode = new Node<T>(data, temp, temp?.Next);
+            temp.Next.Previous = newNode;
+            temp.Next = newNode;
+
+            Size++;
+        }
+
+        public T PeekLast()
+        {
+            if (IsEmpty()) throw new Exception("Trying to access an empty list.");
+            return Tail?.Data;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Node<T> where T : class
+    {
+        public T Data;
+        public Node<T>? Previous;
+        public Node<T>? Next;
+
+        public Node(T data, Node<T>? previous, Node<T>? next)
+        {
+            Data = data;
+            Previous = previous;
+            Next = next;
+        }
+
+        override public string ToString()
+        {
+            return Data?.ToString();
+        }
+    }
+}
